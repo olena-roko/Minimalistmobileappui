b9a6a8c58b5c9caf1452bc97fc7fe333134dcdab
Olena <69715842+olena-roko@users.noreply.github.com>
2026-02-06 00:09:42 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@
   const __dirname = path.dirname(__filename);
 
   export default defineConfig({
+    base: '/Minimalistmobileappui/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
